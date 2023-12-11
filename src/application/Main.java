9e130d174f4a7f1d40fec0f284d6f6ad12b5a9ea
josieying <josieying@localhost>
2023-12-11 14:24:51 +1300

    create the project

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,6 +13,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root, 400, 500);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+
 			primaryStage.show();
 		} catch (Exception e) {
 			e.printStackTrace();
